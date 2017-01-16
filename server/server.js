8ff54aff2d5e55170628e7f7eb4e42c4410ee49a
Hack Reactor Students <accounts@hackreactor.com>
2017-01-16 15:09:42 -0600

    talking with nasa

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const chalk = require('chalk')
+const chalk = require('chalk');
+//const http = require('http'); // not needed?
+const Promise = require('bluebird');
+const https = require('https');
+
 
 const app = express();
 
@@ -9,8 +13,33 @@ app.listen(port, () => {
   console.log(chalk.cyan(`server is now listening on port: ${port}`));
 })
 
+//Query Nasa Promise, in Node.
+// const options = {
+//   host: 'https://api.nasa.gov/planetary/apod?api_key=BmdjxT0wDG093ZC0qM4cihqZMnGvVC22MUiQ4esB'
+// }
+
+const queryNasaApi = () => {
+  console.log(`queryNasaApi is primed`);
+  return new Promise((resolve,reject) =>{
+    console.log(`queryNasaApi is firing`);
+    return https.request('https://api.nasa.gov/planetary/apod?api_key=BmdjxT0wDG093ZC0qM4cihqZMnGvVC22MUiQ4esB', res =>{
+      console.log(`queryNasaApi has fired`);
+      res.setEncoding('utf8');
+      res.on('data', data => {
+        console.log(chalk.yellow(`this is the response from nasa ${data}`))
+        resolve(data);
+      })
+    }).end()
+  })
+}
+
+
+
 //begining route, will move.
 
 app.get('/', (req,res) => {
-    res.end('server is working')
+    queryNasaApi().then(response => {
+      console.log(chalk.green(`queryNasaApi promise is firing - result: ${response}`))
+      res.end(response);
+    })
   });
