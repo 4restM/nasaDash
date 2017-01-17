1901826759a665d1ef87bff172f99a3dfe0b0059
Hack Reactor Students <accounts@hackreactor.com>
2017-01-16 19:41:31 -0600

    added clock, and angular routes dependancy

--- client/app/dash/dash.js
+++ client/app/dash/dash.js
@@ -3,3 +3,15 @@ angular.module('nasadash.dash', [])
 .controller('DashController', function($scope, Dash) {
   
 })
+
+.controller('timeController', function($scope, $timeout){
+  $scope.clock = "loading clock";
+  $scope.tickInterval = 1000; //1 sec
+
+  var tick = function (){
+    $scope.clock = Date.new();
+    $timeout(tick, $scope.tickInterval);
+  }
+
+  $timeout(tick, $scope.tickInterval);
+})
